0c5bd718133645119a75f561efae8ec1ed1aadd0
Anselmo <ansinftec17@gmail.com>
2022-11-02 12:29:08 -0300

    FEAT:Add files initials

---
+++ readme.md
@@ -0,0 +1,15 @@
+# Clima nas Cidades
+<img src="assets/tela.png">
+
+## Aplicação criada com as seguintes tecnologias:
+* Html
+* Css
+* Javascript
+
+
+
+## Informações da tela Criada 
+* Foi uma grid responsivo criado com cards de pokemons onde fazemos uma chamada de api.
+* Com o retorno montamos um grid de dados com retorno dos dados.
+* E com as informações mostramos dados de cada pokemon.
+
---
+++ script/script.js
@@ -0,0 +1,52 @@
+// config api
+const key = '&units=metric&appid=c0248e94dec6f66826cb62438335f605&lang=pt_br';
+const url = `https://api.openweathermap.org/data/2.5/weather?q=`;
+
+// url falf
+const URL_apiBandeira = 'https://countryflagsapi.com/png/';
+
+// variables cards
+const city = document.getElementById("cityCurrent");
+const bandeira = document.getElementById("bandeiraCurrent");
+const grau = document.getElementById("grauCurrent");
+const temp = document.getElementById("tempoCurrent");
+const umidade = document.getElementById("umidadeCurrent");
+const vento = document.getElementById("ventoCurrent");
+
+
+
+async function searchCity() {
+    // value city
+    const citySearch = document.getElementById("city").value;
+
+    // search api 
+    const urlApi = `${url}${citySearch}${key}`;
+    const result = await fetch(urlApi);
+    const findCity = await result.json();
+
+    const sucesso = 200;
+    // add card
+    const hiddenCard = document.querySelector(".hiddenCard");
+    hiddenCard.style = 'display:block';
+    
+    // valid item found
+    const notFound = document.querySelector(".notFound");
+    const found = document.querySelector(".found");
+
+    if (findCity.cod !== sucesso) {
+        notFound.style = 'display:block;';
+        found.style = 'display:none'
+        return;
+    }
+
+    notFound.style = 'display:none;';
+    found.style = 'display:block'
+
+    // add value find 
+    city.innerText = findCity.name;
+    bandeira.setAttribute('src', URL_apiBandeira + findCity.sys.country);
+    grau.innerText = Math.round(findCity.main.temp) + '°C';
+    temp.innerText = findCity.weather[0].description;
+    umidade.innerHTML = findCity.main.humidity + '%';
+    vento.innerHTML = findCity.wind.speed + 'km/h';
+}
